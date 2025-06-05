bad7ab80c9701a15dbddd386b03bbcac8a16438e
Jailed <50757554+Jailed-4667@users.noreply.github.com>
2025-06-05 02:20:46 -0400

    Delete app directory

--- app/utils/dataHandler.js
+++
@@ -1,9 +0,0 @@
-const tempStorage = {};
-
-export const storeData = (key, data) => {
-	tempStorage[key] = data;
-};
-export const clearData = (key) => {
-	delete tempStorage[key];
-};
-export const getData = (key) => tempStorage[key];
